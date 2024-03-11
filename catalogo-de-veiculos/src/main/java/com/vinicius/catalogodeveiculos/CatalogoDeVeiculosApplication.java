fbccc96bcde15424adc2ef9e0bf90d00152a7307
luisHenrickk <83310782+vinisdp@users.noreply.github.com>
2024-03-11 10:03:08 -0400

    feat: Criando projeto spring

---
+++ catalogo-de-veiculos/src/main/java/com/vinicius/catalogodeveiculos/CatalogoDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.catalogodeveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ catalogo-de-veiculos/src/test/java/com/vinicius/catalogodeveiculos/CatalogoDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.catalogodeveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
