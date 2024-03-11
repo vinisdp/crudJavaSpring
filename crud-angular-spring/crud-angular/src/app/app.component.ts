c2bd28699d63469906d0c27f4a0d5fd44289dd29
luisHenrickk <83310782+vinisdp@users.noreply.github.com>
2024-03-11 15:17:10 -0400

    Criando toolbar material

--- crud-angular-spring/crud-angular/src/app/app.component.ts
+++ crud-angular-spring/crud-angular/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'crud-angular';
