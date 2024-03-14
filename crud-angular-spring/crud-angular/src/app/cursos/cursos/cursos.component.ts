b09ab7dc8305abc45d54cf24bf893df4178aaee7
luisHenrickk <83310782+vinisdp@users.noreply.github.com>
2024-03-14 16:28:06 -0400

    feat: adicionando carregamento com ngIf

--- crud-angular-spring/crud-angular/src/app/cursos/cursos/cursos.component.ts
+++ crud-angular-spring/crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -5,6 +5,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 
 import { CursosService } from '../services/cursos.service';
 import { Observable } from 'rxjs';
@@ -19,16 +20,17 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    CommonModule,
   ],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss',
 })
 export class CursosComponent {
-  cursos: Observable<Curso[]>;
+  cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
   constructor(private cursosService: CursosService) {
-    this.cursos = this.cursosService.list();
+    this.cursos$ = this.cursosService.list();
   }
   NgOnInit(): void {}
 }
--- crud-angular-spring/crud-angular/src/app/cursos/services/cursos.service.ts
+++ crud-angular-spring/crud-angular/src/app/cursos/services/cursos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Curso } from '../models/curso';
 import { HttpClient } from '@angular/common/http';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,7 @@ export class CursosService {
   list() {
     return this.httpClient.get<Curso[]>(this.API).pipe(
       first(),
+      delay(5000),
       tap((cursos) => console.log(cursos))
     );
   }
